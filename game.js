9846ede8e8d96a754e70763e131525ef2ab5e1b2
Yukai Huang <yukaihuangtw@gmail.com>
2023-05-06 16:43:03 +0800

    feat: add track

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+let trackOffsetX = 0;
+let trackOffsetY = 0;
+
 function gameLoop() {
   update();
   render();
@@ -12,6 +15,24 @@ class Kart {
     this.x = x;
     this.y = y;
     this.angle = angle;
+    this.velocity = 0;
+    this.acceleration = 0.05;
+  }
+
+  moveForward() {
+    this.velocity += this.acceleration;
+  }
+
+  moveBackward() {
+    this.velocity -= this.acceleration;
+  }
+
+  turnLeft() {
+    this.angle -= 0.05;
+  }
+
+  turnRight() {
+    this.angle += 0.05;
   }
 
   update() {
@@ -35,25 +56,83 @@ class Kart {
 
 const kart = new Kart(400, 300, 0);
 
+class Track {
+  constructor() {
+    this.gridSize = 50;
+  }
+
+  render(offsetX, offsetY) {
+    ctx.strokeStyle = "gray";
+    ctx.lineWidth = 1;
+
+    for (
+      let i = -offsetX % this.gridSize;
+      i < canvas.width;
+      i += this.gridSize
+    ) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, canvas.height);
+      ctx.stroke();
+    }
+
+    for (
+      let j = -offsetY % this.gridSize;
+      j < canvas.height;
+      j += this.gridSize
+    ) {
+      ctx.beginPath();
+      ctx.moveTo(0, j);
+      ctx.lineTo(canvas.width, j);
+      ctx.stroke();
+    }
+  }
+}
+
+const track = new Track();
+
+const keys = {};
 
 window.addEventListener('keydown', (e) => {
-    // Handle keydown events for controlling the kart
+    keys[e.key] = true;
 });
 
 window.addEventListener('keyup', (e) => {
-    // Handle keyup events for controlling the kart
+    keys[e.key] = false;
 });
 
+function handleInput() {
+    if (keys['ArrowUp']) {
+        kart.moveForward();
+    }
+    if (keys['ArrowDown']) {
+        kart.moveBackward();
+    }
+    if (keys['ArrowLeft']) {
+        kart.turnLeft();
+    }
+    if (keys['ArrowRight']) {
+        kart.turnRight();
+    }
+}
 
 function update() {
-  // Update kart position, drifting, N2O, etc.
+  handleInput();
+  const dx = Math.cos(kart.angle) * kart.velocity;
+  const dy = Math.sin(kart.angle) * kart.velocity;
+
+  // Move the terrain instead of the kart
+  trackOffsetX -= dx;
+  trackOffsetY -= dy;
+
   kart.update();
 }
 
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  track.render(trackOffsetX, trackOffsetY);
   kart.render();
 }
-
 
 gameLoop();
